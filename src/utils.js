0c4ff6236f1d69eac9c0291fc773a8207ae2e533
chengkai <17701523921@163.com>
2017-11-07 16:54:54 +0800

    更新jq工具方法

--- src/utils.js
+++ src/utils.js
@@ -7,5 +7,28 @@ export default {
         return typeof obj !== 'object'?
             typeof obj:
             Object.prototype.toString.call(obj)
-    }
+    },
+    //each遍历
+    each: function(obj, callback){
+        var length,
+            i = 0;
+
+        if(jq.type(obj) === '[object Array]'){
+            length = obj.length;
+
+            for(; i<length; i++) {
+                if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
+					break;
+				}
+            }
+        } else if(jq.type(obj) === '[object Object]') {
+			for ( i in obj ) {
+				if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
+					break;
+				}
+			}
+        } else {
+            return obj
+        }
+    },
 }
