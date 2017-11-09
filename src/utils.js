2d5ab985421ba0d65cad533a91ea16dff7d6156d
chengkai <17701523921@163.com>
2017-11-09 16:32:59 +0800

    新增工具方法 dir

--- src/utils.js
+++ src/utils.js
@@ -13,22 +13,54 @@ export default {
         var length,
             i = 0;
 
-        if(jq.type(obj) === '[object Array]'){
-            length = obj.length;
+        if(jq.type(obj) === '[object object]'){
 
-            for(; i<length; i++) {
-                if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
-					break;
-				}
-            }
-        } else if(jq.type(obj) === '[object Object]') {
-			for ( i in obj ) {
+            for ( i in obj ) {
 				if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
 					break;
 				}
 			}
+
         } else {
-            return obj
+            length = obj.length;
+            
+            for(; i<length; i++) {
+                if ( callback.call( obj[ i ], i, obj[ i ] ) === false ) {
+                    break;
+                }
+            }
         }
     },
+    //转化成数组对象
+    toArray: function() {
+		return core_slice.call( this, 0 );
+    },
+    get: function( num ) {
+        return num == null ?
+        
+            this.toArray() :
+            
+			( num < 0 ? this[ this.length + num ] : this[ num ] );
+    },
+    //从一个元素出发，查找某个方向上的所有元素
+    dir: function( nodes, elem, dir ){
+        var matched = [];
+
+        this.each( nodes, function( k, v ){
+            while ( (v = v[dir]) && v.nodeType !== 9 ) {
+                if( [].slice.call(jq(elem)).indexOf(v) !== -1 ){
+                    matched = [];
+                    matched.push( v );
+                    break
+                }
+                if( v.nodeType ===1 && matched.indexOf(v) === -1 ) {
+                    matched.push( v )
+                }
+            }
+
+        });
+        
+        matched.__proto__ = jq.fn;
+        return matched
+    }
 }
